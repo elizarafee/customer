d75cba03cc04fe4e89a667ef0ade2f515d3395e9
Eliza Ahmed <elizarafee@gmail.com>
2021-02-17 16:43:02 +0000

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './components/home';
 import About from './components/about';
 import Navbar from './components/navbar';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
