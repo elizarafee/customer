bfd778e2e16cf2d9f0a1a46766fdc4a8735da14a
Eliza Ahmed <elizarafee@gmail.com>
2021-02-17 16:26:12 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './components/home';
 import About from './components/about';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
       <About />
     </div>
