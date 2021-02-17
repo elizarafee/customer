6f14064c4675f24a40f1b5eb32e5c74b5dfb5696
Eliza Ahmed <elizarafee@gmail.com>
2021-02-17 15:56:43 +0000

    home component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
-      hello World
+      <Home />
     </div>
   );
 }
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1 className="title is-1">Home</h1>
+    <p>
+        This is the Home Page
+    </p>
+  </div>
+);
+
+export default Home;
