8c920c3bd1456f4c44aa4960ecece7092004a4de
Eliza Ahmed <elizarafee@gmail.com>
2021-02-17 15:57:10 +0000

    about page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './components/home';
+import About from './components/about';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h1 className="title is-1">About</h1>
+    <p>
+      This is the About Page
+    </p>
+  </div>
+);
+
+export default About;
